6974dd9d1a60eb6da0760c7ab441c0a9ff565b24
Andreas Wälchli <andreas.waelchli@me.com>
2021-01-27 10:23:23 +0000

    maven setup

---
+++ src/main/java/ch/awae/minecraft/discordchat/McDiscordChatApplication.java
@@ -0,0 +1,13 @@
+package ch.awae.minecraft.discordchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McDiscordChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McDiscordChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/awae/minecraft/discordchat/McDiscordChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.awae.minecraft.discordchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McDiscordChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
