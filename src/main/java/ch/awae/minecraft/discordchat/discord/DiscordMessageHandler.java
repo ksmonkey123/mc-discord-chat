8407f7cc913bc759680e1124aee9e045df62fd00
Andreas Wälchli <andreas.waelchli@me.com>
2021-01-28 00:37:40 +0000

    added line wrapping: discord has a message limit of 2000 characters. But minecraft chat (starting with 1.11) has a limit of 255 characters.
    Consequently, if we forward from minecraft to discord, we don't have any issues. But if we send messages from discord to minecraft, we need to break them up into multiple messages. Also discord messages can have line breaks and empty lines in them. Minecraft does not support that. Multi-line messages are split into multiple single-line messages and empty lines are ignored.

--- src/main/java/ch/awae/minecraft/discordchat/discord/DiscordMessageHandler.java
+++ src/main/java/ch/awae/minecraft/discordchat/discord/DiscordMessageHandler.java
@@ -4,17 +4,21 @@ import ch.awae.minecraft.discordchat.minecraft.MinecraftSendingService;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
+import org.apache.commons.text.WordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class DiscordMessageHandler implements EventListener<MessageCreateEvent> {
 
-    private static Logger log = Logger.getLogger(DiscordMessageHandler.class.getName());
+    private static final Logger log = Logger.getLogger(DiscordMessageHandler.class.getName());
 
     private final DiscordConfig config;
     private final ExecutorService async;
@@ -41,14 +45,27 @@ public class DiscordMessageHandler implements EventListener<MessageCreateEvent> 
                 .map(MessageCreateEvent::getMessage)
                 .filter(message -> config.getChannelId().equals(message.getChannelId()))
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .flatMap(this::logMessage)
+                .flatMap(this::forwardMessage)
                 .then();
     }
 
-    private Mono<?> logMessage(Message message) {
+    private Mono<?> forwardMessage(Message message) {
         String author = message.getAuthor().map(User::getUsername).orElse(null);
         log.info(author + ": " + message.getContent());
-        async.submit(() -> service.send(author, message.getContent()));
+
+        List<String> lines = Stream.of(message.getContent().split("\n"))
+                .map(line -> WordUtils.wrap(line, 255))
+                .map(line -> line.split("\n"))
+                .flatMap(Stream::of)
+                .filter(line -> line.length() > 0)
+                .collect(Collectors.toList());
+
+        async.submit(() -> {
+            for (String line : lines) {
+                service.send(author, line);
+            }
+        });
+
         return Mono.empty();
     }
 }
